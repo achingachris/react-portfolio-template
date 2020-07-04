32680d2833839943341df03521cd614a1cfd4a4d
ChrisAchinga <achinga.chris@gmail.com>
2020-07-04 13:39:50 +0300

    form action

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -36,7 +36,7 @@ class Contact extends Component {
          <div className="row">
             <div className="eight columns">
 
-               <form action="" method="post" id="contactForm" name="contactForm">
+               <form action="maito:achinga.chris@gmail.com" method="post" id="contactForm" name="contactForm">
 					<fieldset>
 
                   <div>
